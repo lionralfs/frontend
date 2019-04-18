17b64bc23bdafbd2e70f006eb5930b91b0a18111
Lion Ralfs <lion.ralfs@hotmail.com>
2019-04-18 15:38:20 +0200

    feat(layout): init page structure, added map

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
-InitChart();
+(async () => {
+  const rawData = await d3.csv(
+    'https://gist.githubusercontent.com/mbostock/14613fb82f32f40119009c94f5a46d72/raw/d0d70ffb7b749714e4ba1dece761f6502b2bdea2/aapl.csv',
+    d3.autoType
+  );
 
-function InitChart() {
-  var lineData = [
+  const dummyData = [
     {
       x: 1,
       y: 5
@@ -28,45 +31,68 @@ function InitChart() {
     }
   ];
 
-  var vis = d3.select('#graph'),
-    WIDTH = 1000,
-    HEIGHT = 500,
-    MARGINS = {
-      top: 20,
-      right: 20,
-      bottom: 20,
-      left: 50
-    },
-    xRange = d3
-      .scaleLinear()
-      .range([MARGINS.left, WIDTH - MARGINS.right])
-      .domain([
-        d3.min(lineData, function(d) {
-          return d.x;
-        }),
-        d3.max(lineData, function(d) {
-          return d.x;
-        })
-      ]),
-    yRange = d3
-      .scaleLinear()
-      .range([HEIGHT - MARGINS.top, MARGINS.bottom])
-      .domain([
-        d3.min(lineData, function(d) {
-          return d.y;
-        }),
-        d3.max(lineData, function(d) {
-          return d.y;
-        })
-      ]),
-    xAxis = d3
-      .axisBottom()
-      .scale(xRange)
-      .tickSize(5),
-    yAxis = d3
-      .axisLeft()
-      .scale(yRange)
-      .tickSize(5);
+  const stockData = rawData.map(({ close, date }) => {
+    // console.dir(date);
+    return {
+      x: date,
+      y: close
+    };
+  });
+
+  InitChart(stockData);
+})();
+
+function InitChart(lineData) {
+  const vis = d3
+    .select('#chart-1')
+    .append('svg')
+    .attr('preserveAspectRatio', 'xMinYMin meet')
+    .attr('viewBox', '0 0 485.4 300')
+    .classed('svg-content', true);
+
+  const WIDTH = 485.4;
+  const HEIGHT = 300;
+
+  const MARGINS = {
+    top: 20,
+    right: 20,
+    bottom: 20,
+    left: 50
+  };
+
+  const xRange = d3
+    .scaleLinear()
+    .range([MARGINS.left, WIDTH - MARGINS.right])
+    .domain([
+      d3.min(lineData, function(d) {
+        return d.x;
+      }),
+      d3.max(lineData, function(d) {
+        return d.x;
+      })
+    ]);
+
+  const yRange = d3
+    .scaleLinear()
+    .range([HEIGHT - MARGINS.top, MARGINS.bottom])
+    .domain([
+      d3.min(lineData, function(d) {
+        return d.y;
+      }),
+      d3.max(lineData, function(d) {
+        return d.y;
+      })
+    ]);
+
+  const xAxis = d3
+    .axisBottom()
+    .scale(xRange)
+    .tickSize(5);
+
+  const yAxis = d3
+    .axisLeft()
+    .scale(yRange)
+    .tickSize(5);
 
   vis
     .append('svg:g')
@@ -80,7 +106,7 @@ function InitChart() {
     .attr('transform', 'translate(' + MARGINS.left + ',0)')
     .call(yAxis);
 
-  var lineFunc = d3
+  const lineFunc = d3
     .line()
     .x(function(d) {
       return xRange(d.x);
@@ -97,3 +123,8 @@ function InitChart() {
     .attr('stroke-width', 2)
     .attr('fill', 'none');
 }
+
+const map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
