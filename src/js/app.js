201f975607856b741aaece71a6c7423f010196fa
moritzkreinsen <moritzkreinsen@outlook.com>
2019-06-25 13:45:13 +0200

    open/close sidebar

--- src/js/app.js
+++ src/js/app.js
@@ -5,31 +5,68 @@ import { initRangeSlider } from './range-slider';
 import { initAirChart, prepareDataForChart } from './air-chart';
 import inside from 'point-in-polygon';
 
-(async () => {
-  let airChart;
-  let sliderPosition = 0;
+(async() => {
+    let airChart;
+    let sliderPosition = 0;
 
-  const airData = await fetchData();
+    const airData = await fetchData();
 
-  const heatmap = initMap(airData[sliderPosition], function visibleAreaChanged(event) {
-    const bounds = event.target.getBounds();
-    const southWest = bounds._southWest;
-    const northEast = bounds._northEast;
-    const polygon = [[southWest.lng, southWest.lat], [southWest.lng, northEast.lat], [northEast.lng, northEast.lat], [northEast.lng, southWest.lat]];
-    const filtered = airData.map(function(interval) {
-      return interval.filter(function(measurement) {
-        return inside([measurement.lng, measurement.lat], polygon);
-      });
+    const heatmap = initMap(airData[sliderPosition], function visibleAreaChanged(event) {
+        const bounds = event.target.getBounds();
+        const southWest = bounds._southWest;
+        const northEast = bounds._northEast;
+        const polygon = [
+            [southWest.lng, southWest.lat],
+            [southWest.lng, northEast.lat],
+            [northEast.lng, northEast.lat],
+            [northEast.lng, southWest.lat]
+        ];
+        const filtered = airData.map(function(interval) {
+            return interval.filter(function(measurement) {
+                return inside([measurement.lng, measurement.lat], polygon);
+            });
+        });
+        const data = prepareDataForChart(filtered);
+        airChart.onDataChange(data);
     });
-    const data = prepareDataForChart(filtered);
-    airChart.onDataChange(data);
-  });
 
-  initRangeSlider(function onChange(i) {
-    sliderPosition = Math.min(i, 23);
-    airChart.onSliderChange(23 - sliderPosition);
-    heatmap.setData({ data: airData[sliderPosition] });
-  });
+    initRangeSlider(function onChange(i) {
+        sliderPosition = Math.min(i, 23);
+        airChart.onSliderChange(23 - sliderPosition);
+        heatmap.setData({ data: airData[sliderPosition] });
+    });
 
-  airChart = initAirChart(prepareDataForChart(airData));
+    airChart = initAirChart(prepareDataForChart(airData));
 })();
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px 
+function openSidebar() {
+    document.getElementById("sidebar").style.width = "40%";
+    document.getElementById("openbtn").style.marginLeft = "250px";
+}*/
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 
+function closeSidebar() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("openbtn").style.marginLeft = "0";
+}*/
+
+document.getElementById("openbtn").addEventListener("click", toggleSidebar);
+
+function toggleSidebar() {
+    let sidebarSize = document.getElementById("sidebar").style.width;
+    if (sidebarSize == "50%") {
+        return closeSidebar();
+    }
+    return openSidebar();
+}
+
+function openSidebar() {
+    document.getElementById("sidebar").style.width = "50%";
+    document.getElementById("map-wrapper").style.width = "50%";
+}
+
+function closeSidebar() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("map-wrapper").style.width = "100%";
+}
