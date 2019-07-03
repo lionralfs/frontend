8fd54f202bc4332c571169ee978484694221c2a9
moritzkreinsen <moritzkreinsen@outlook.com>
2019-07-03 13:51:57 +0200

    change button-style for open/close function

--- src/js/app.js
+++ src/js/app.js
@@ -65,10 +65,12 @@ function openSidebar() {
     document.getElementById("sidebar").style.width = "50%";
     document.getElementById("map-wrapper").style.width = "50%";
     document.getElementById("map").style.width = "200%";
+    document.getElementById("openbtn").innerHTML = '&#10005';
 }
 
 function closeSidebar() {
     document.getElementById("sidebar").style.width = "0";
     document.getElementById("map-wrapper").style.width = "100%";
     document.getElementById("map").style.width = "100%";
+    document.getElementById("openbtn").innerHTML = '&#9776;';
 }
