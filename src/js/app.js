348e49acd6bc36f5d7bb5eb835a49840b55f848d
moritzkreinsen <moritzkreinsen@outlook.com>
2019-10-11 17:07:39 +0200

    added leaflet locate control

--- src/js/app.js
+++ src/js/app.js
@@ -2,93 +2,95 @@ import '../css/style.css';
 import 'js-datepicker/dist/datepicker.min.css';
 import { getHeatmapForTimestamp } from './data';
 import { initMap } from './air-map';
+import 'leaflet.locatecontrol/dist/L.Control.Locate.css';
+import 'leaflet.locatecontrol'; //HOW TO IMPORT THE LOCATECONTROL?
 import { initRangeSlider } from './range-slider';
 import { initAirChart, prepareDataForChart } from './air-chart';
 import inside from 'point-in-polygon';
 import datepicker from 'js-datepicker';
 
 function dateToTimestampInSeconds(date) {
-  return Math.floor(date.getTime() / 1000);
+    return Math.floor(date.getTime() / 1000);
 }
 
 function getTimestampsFromDate(date) {
-  const start = dateToTimestampInSeconds(date);
-  const timestamps = [];
-  for (let i = 0; i < 25; i++) {
-    timestamps[i] = start + i * 3600;
-  }
-  return timestamps;
+    const start = dateToTimestampInSeconds(date);
+    const timestamps = [];
+    for (let i = 0; i < 25; i++) {
+        timestamps[i] = start + i * 3600;
+    }
+    return timestamps;
 }
 
 async function getDataForEntireDay(listOfTimestamps, type) {
-  const result = [];
+    const result = [];
 
-  for (const timestamp of listOfTimestamps) {
-    const data = await getHeatmapForTimestamp(timestamp, type);
-    result.push(data);
-  }
-  
-  return result;
+    for (const timestamp of listOfTimestamps) {
+        const data = await getHeatmapForTimestamp(timestamp, type);
+        result.push(data);
+    }
+
+    return result;
 }
 
-(async () => {
-  const now = new Date();
-  const today = new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate()));
+(async() => {
+    const now = new Date();
+    const today = new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate()));
 
-  let type = 'p10';
-  let airChart;
-  let sliderPosition = now.getUTCHours();
+    let type = 'p10';
+    let airChart;
+    let sliderPosition = now.getUTCHours();
 
-  let timestamps = getTimestampsFromDate(today);
-  let cached = await getDataForEntireDay(timestamps, type);
+    let timestamps = getTimestampsFromDate(today);
+    let cached = await getDataForEntireDay(timestamps, type);
 
-  const heatmap = initMap(cached[sliderPosition], function visibleAreaChanged(event) {
-    // const bounds = event.target.getBounds();
-    // const southWest = bounds._southWest;
-    // const northEast = bounds._northEast;
-    // const polygon = [
-    //     [southWest.lng, southWest.lat],
-    //     [southWest.lng, northEast.lat],
-    //     [northEast.lng, northEast.lat],
-    //     [northEast.lng, southWest.lat]
-    // ];
-    // const filtered = airData.map(function(interval) {
-    //     return interval.filter(function(measurement) {
-    //         return inside([measurement.lng, measurement.lat], polygon);
-    //     });
-    // });
-    // const data = prepareDataForChart(filtered);
-    // airChart.onDataChange(data);
-  });
+    const heatmap = initMap(cached[sliderPosition], function visibleAreaChanged(event) {
+        // const bounds = event.target.getBounds();
+        // const southWest = bounds._southWest;
+        // const northEast = bounds._northEast;
+        // const polygon = [
+        //     [southWest.lng, southWest.lat],
+        //     [southWest.lng, northEast.lat],
+        //     [northEast.lng, northEast.lat],
+        //     [northEast.lng, southWest.lat]
+        // ];
+        // const filtered = airData.map(function(interval) {
+        //     return interval.filter(function(measurement) {
+        //         return inside([measurement.lng, measurement.lat], polygon);
+        //     });
+        // });
+        // const data = prepareDataForChart(filtered);
+        // airChart.onDataChange(data);
+    });
 
-  const picker = datepicker('.date-select', {
-    onSelect: async (instance, date) => {
-      if (!date) {
-        date = today;
-      }
-      const selectedDay = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
-      console.log(selectedDay);
-      timestamps = getTimestampsFromDate(selectedDay);
-      cached = await getDataForEntireDay(timestamps, type);
+    const picker = datepicker('.date-select', {
+        onSelect: async(instance, date) => {
+            if (!date) {
+                date = today;
+            }
+            const selectedDay = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+            console.log(selectedDay);
+            timestamps = getTimestampsFromDate(selectedDay);
+            cached = await getDataForEntireDay(timestamps, type);
 
-      heatmap.setData({ data: cached[sliderPosition], max: 500 });
-    }
-  });
-  picker.setDate(today, true);
+            heatmap.setData({ data: cached[sliderPosition], max: 500 });
+        }
+    });
+    picker.setDate(today, true);
 
-  initRangeSlider(sliderPosition, async function onChange(i) {
-    sliderPosition = i;
-    heatmap.setData({ data: cached[sliderPosition], max: 500 });
-  });
+    initRangeSlider(sliderPosition, async function onChange(i) {
+        sliderPosition = i;
+        heatmap.setData({ data: cached[sliderPosition], max: 500 });
+    });
 
-  document.querySelector('.type-select').addEventListener('change', async function(evt) {
-      type = evt.target.value;
+    document.querySelector('.type-select').addEventListener('change', async function(evt) {
+        type = evt.target.value;
 
-      cached = await getDataForEntireDay(timestamps, type);
-      heatmap.setData({ data: cached[sliderPosition], max: 500 });
-  });
+        cached = await getDataForEntireDay(timestamps, type);
+        heatmap.setData({ data: cached[sliderPosition], max: 500 });
+    });
 
-  // airChart = initAirChart(prepareDataForChart(airData));
+    // airChart = initAirChart(prepareDataForChart(airData));
 })();
 
 document.getElementById('openbtn').addEventListener('click', toggleSidebar);
@@ -99,47 +101,53 @@ document.querySelector('.mobile-open').addEventListener('click', toggleSideBarMo
 document.querySelector('.mobile-close').addEventListener('click', toggleSideBarMobile);
 
 function toggleSideBarMobile() {
-  document.querySelector('.sidebar').classList.toggle('open');
+    document.querySelector('.sidebar').classList.toggle('open');
 }
 
 function toggleSidebar() {
-  let sidebarSize = document.getElementById('sidebar').style.width;
-  if (sidebarSize == '30%') {
-    return closeSidebar();
-  }
-  return openSidebar();
+    let sidebarSize = document.getElementById('sidebar').style.width;
+    if (sidebarSize == '30%') {
+        return closeSidebar();
+    }
+    return openSidebar();
 }
 
 /*
 Opens/closes the sidebar
 */
 function openSidebar() {
-  document.getElementById('sidebar').style.width = '30%';
-  document.getElementById('map-wrapper').style.width = '70%';
-  document.getElementById('navbar').style.width = '70%';
-  document.getElementById('map').style.width = '200%';
-  document.getElementById('openbtn').innerHTML = '&#10005';
+    document.getElementById('sidebar').style.width = '30%';
+    document.getElementById('map-wrapper').style.width = '70%';
+    document.getElementById('navbar').style.width = '70%';
+    document.getElementById('map').style.width = '200%';
+    document.getElementById('openbtn').innerHTML = '&#10005';
 }
 
 function closeSidebar() {
-  document.getElementById('sidebar').style.width = '0';
-  document.getElementById('map-wrapper').style.width = '100%';
-  document.getElementById('navbar').style.width = '100%';
-  document.getElementById('map').style.width = '200%';
-  document.getElementById('openbtn').innerHTML = '&#9776;';
+    document.getElementById('sidebar').style.width = '0';
+    document.getElementById('map-wrapper').style.width = '100%';
+    document.getElementById('navbar').style.width = '100%';
+    document.getElementById('map').style.width = '200%';
+    document.getElementById('openbtn').innerHTML = '&#9776;';
 }
 
 function showAboutTextbox() {
-  hideTextbox();
-  document.getElementById('about').style.display = 'block';
+    hideTextbox();
+    document.getElementById('about').style.display = 'block';
 }
 
 function showContactTextbox() {
-  hideTextbox();
-  document.getElementById('contact').style.display = 'block';
+    hideTextbox();
+    document.getElementById('contact').style.display = 'block';
 }
 
 function hideTextbox() {
-  document.getElementById('contact').style.display = 'none';
-  document.getElementById('about').style.display = 'none';
+    document.getElementById('contact').style.display = 'none';
+    document.getElementById('about').style.display = 'none';
 }
+
+// create control and add to map
+var lc = L.control.locate().addTo(map);
+
+// request location update and set location
+lc.start();
