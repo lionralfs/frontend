215bcf97ad2a604a960f9c0730f025232e3f08a5
Lion Ralfs <lion.ralfs@gmail.com>
2019-10-18 22:02:28 +0200

    change valuefield description (#48)

--- src/js/air-map.js
+++ src/js/air-map.js
@@ -4,45 +4,43 @@ import 'leaflet/dist/leaflet.css';
 import { updateLegend } from './legend';
 
 const cfg = {
-    // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-    // if scaleRadius is false it will be the constant radius used in pixels
-    radius: 0.1,
-    maxOpacity: 0.5,
-    blur: 1,
-    // scales the radius based on map zoom
-    scaleRadius: true,
-    // if set to false the heatmap uses the global maximum for colorization
-    // if activated: uses the data maximum within the current map boundaries
-    //   (there will always be a red spot with useLocalExtremas true)
-    useLocalExtrema: false,
-    // which field name in your data represents the latitude - default "lat"
-    latField: 'y',
-    // which field name in your data represents the longitude - default "lng"
-    lngField: 'x',
-    // which field name in your data represents the data value - default "value"
-    valueField: 'value',
-    onExtremaChange: function(data) {
-        updateLegend(data);
-    }
+  // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+  // if scaleRadius is false it will be the constant radius used in pixels
+  radius: 0.1,
+  maxOpacity: 0.5,
+  blur: 1,
+  // scales the radius based on map zoom
+  scaleRadius: true,
+  // if set to false the heatmap uses the global maximum for colorization
+  // if activated: uses the data maximum within the current map boundaries
+  //   (there will always be a red spot with useLocalExtremas true)
+  useLocalExtrema: false,
+  // which field name in your data represents the latitude - default "lat"
+  latField: 'y',
+  // which field name in your data represents the longitude - default "lng"
+  lngField: 'x',
+  // which field name in your data represents the data value - default "value"
+  valueField: 'v',
+  onExtremaChange: updateLegend
 };
 
 export function initMap(airData, onVisibleAreaChanged) {
-    console.log(airData);
-    const osmTileLayer = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
-    const heatmapLayer = new HeatmapOverlay(cfg);
+  console.log(airData);
+  const osmTileLayer = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
+  const heatmapLayer = new HeatmapOverlay(cfg);
 
-    const leafletMap = map('map', {
-        center: [53.551086, 9.993682],
-        zoom: 5,
-        minZoom: 3,
-        layers: [osmTileLayer, heatmapLayer]
-    });
+  const leafletMap = map('map', {
+    center: [53.551086, 9.993682],
+    zoom: 5,
+    minZoom: 3,
+    layers: [osmTileLayer, heatmapLayer]
+  });
 
-    leafletMap.on('zoomend', onVisibleAreaChanged);
-    leafletMap.on('moveend', onVisibleAreaChanged);
-    heatmapLayer.setData({ data: airData, max: 100 });
+  leafletMap.on('zoomend', onVisibleAreaChanged);
+  leafletMap.on('moveend', onVisibleAreaChanged);
+  heatmapLayer.setData({ data: airData, max: 100 });
 
-    return heatmapLayer;
+  return heatmapLayer;
 }
